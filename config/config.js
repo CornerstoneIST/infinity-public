3d44de299c7b0588b6bc1b190e1d0bd8230d668e
Illya Klymov <xanf@xanf.me>
2013-08-16 19:39:12 +0300

    Add redirect URL

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 exports.config = {
   mongodbUrl : 'mongodb://heroku_app14220631:sa8fb17r1tvvijse1atvnrasbl@ds063177.mongolab.com:63177/heroku_app14220631',
+  adminUrl: 'http://admin.wuzy.com',
   mail: {
     from: 'wuzy.mailer@gmail.com',
     pass: 'GxxInCME4Y',
